e8f1703ad8f97713134a8d9ac3ad176dd73da68e
aspjava <aspjava@users.noreply.github.com>
2025-10-14 15:05:09 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -36,18 +36,6 @@
             console.log('patient.name[0]', patient.name);
 
             fname = patient.name[0].given.join(' ');
-            
-            // 10-14-25
-            // patient.name[0].given is an array but patient.name[0].family is not.        
-            //
-            /*
-            if( Array.isArray(patient.name[0].family) ){
-              lname = patient.name[0].family.join(' ');
-            }
-            else{
-              lname = patient.name[0].family;
-            }
-            */
             lname = patient.name[0].family;
           }
 
