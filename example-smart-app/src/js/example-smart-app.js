bc30335e17277d52ec86fc5e8e2b7c1dc816c74b
aspjava <aspjava@users.noreply.github.com>
2025-10-14 14:27:47 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -32,6 +32,8 @@
           var lname = '';
 
           if (typeof patient.name[0] !== 'undefined') {
+            console.log('patient.name[0]', patient.name);
+            
             fname = patient.name[0].given.join(' ');
             lname = patient.name[0].family.join(' ');
           }
